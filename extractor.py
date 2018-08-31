44e5b2573c6674ca97fb52736bc21ba5fbc7b678
madhavanmalolan <madhavan@madhavanmalolan.com>
2018-08-31 07:52:13 +0530

    First full list

--- extractor.py
+++ extractor.py
@@ -3,20 +3,59 @@ import urllib.request
 import json
 import datetime
 from dateutil import parser
-
-fout = open('README.md','w')
+from credentials import creds
+import sys
+import time
+ferr = open('errors.txt','w',encoding='utf-8')
+fout = open('README.md','w', encoding='utf-8')
 fout.write("# Awesome React Native UI Components\n");
+def curl(url, retry=True):
+    try:
+        """"
+        p = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+        p.add_password(None, url, creds['username'], creds['password'])
+
+        auth_handler = urllib.request.HTTPBasicAuthHandler(p)
+
+        opener = urllib.request.build_opener(auth_handler)
+        urllib.request.install_opener(opener)    
+        o = opener.open(url)
+        return o.read()
+        """
+        return urllib.request.urlopen(url).read()
+    except Exception as e:
+        print(url)
+        print(e)
+        
+
+def getRank(image):
+    if 'shield' in image or 'badge' in image:
+        return 0
+    if ".gif" in image:
+        return 10
+    if ".png" in image:
+        return 5
+    if ".jpg" in image or ".jpeg" in image:
+        return 3
+    if ".svg" in image:
+        return 2
+    return 1
 
 def getImage(repo):
     base = "https://raw.githubusercontent.com/"+repo+    "/master/"
-    md = urllib.request.urlopen(base+"README.md").read()
-    images = re.findall("\!\[.*?\]\((.*?)\)",str(md))
-    print(images)
-    images = sorted(images, key=lambda image : 1 if image.endswith('.gif') else 0)
+    md = curl(base+"README.md")
+    images1 = re.findall("\!\[.*?\]\((.*?)\)",str(md))
+    images2 = re.findall("<img.*?src=[\'\"](.*?)[\'\"]",str(md))
+    images = images1+images2
+    images = sorted(images, key=lambda image : getRank(image), reverse=True)
     if len(images) == 0:
         return ''
     image = images[0]
-    return image if image.startswith("http://") or image.startswith("https://") else base+image
+    finalImage = image if image.startswith("http://") or image.startswith("https://") else base+image
+    print(repo +" : "+finalImage)
+    return finalImage
+
+
 
 def getAgoString(z):
     d = parser.parse(z)
@@ -33,30 +72,34 @@ def getAgoString(z):
 
 def getRepoInfo(repo):
     response = {'stars':'','lastUpdate':'','issues':'', 'name':'','description':'', 'image':''}
+    info = json.loads(curl('https://api.github.com/repos/'+repo))
+    response['stars'] = str(info['stargazers_count'])
+    response['lastUpdate'] = getAgoString(info['updated_at'])
+    response['issues'] = str(info['open_issues'])
+    response['name'] = info['name']
+    response['description'] = info['description']
+    response['image'] = getImage(repo)
+    return response
+
+count = 0;
+print(sys.argv[1])
+for line in open(sys.argv[1]).readlines():
     try:
-        info = json.loads(urllib.request.urlopen('https://api.github.com/repos/'+repo).read())
-        response['stars'] = str(info['stargazers_count'])
-        response['lastUpdate'] = getAgoString(info['updated_at'])
-        response['issues'] = str(info['open_issues'])
-        response['name'] = info['name']
-        response['description'] = info['description']
-        response['image'] = getImage(repo)
-        return response
-
-    except Exception as e:
-        print(e)
-        return response
-
-
-for line in open('components.txt').readlines():
-    if line.startswith("#"):
-        fout.write("\n\n#"+line+"\n\n")
-        fout.write("|Repository | Activity | Demo|\n")
-        fout.write("|---|---|---|\n")
-    elif line.startswith('-'):
-        repo,img = re.findall("\[(.*)\]\((.*)\)",line)[0]
-        
-        i = getRepoInfo(repo)
-        fout.write("|<h3>"+i['name']+"</h3>"+i['description']+"|<ul><li>Last updated : "+i['lastUpdate']+"</li><li>Stars : "+i['stars']+"</li><li>Open issues : "+i['issues']+"</li></ul>|![]("+i['image']+")|\n")
+        if line.startswith("#"):
+            fout.write("\n\n#"+line+"\n\n")
+            fout.write("|Repository | Activity | Demo|\n")
+            fout.write("|---|---|---|\n")
+        elif line.startswith('-'):
+            print(count)
+            count+=1
+            repo,img = re.findall("\[(.*)\]\((.*)\)",line)[0]
+            
+            i = getRepoInfo(repo)
+            fout.write("|[<h3>"+i['name']+"</h3>](https://github.com/"+repo+")"+i['description']+"|<ul><li>Last updated : "+i['lastUpdate']+"</li><li>Stars : "+i['stars']+"</li><li>Open issues : "+i['issues']+"</li></ul>|![]("+i['image']+")|\n")
+        time.sleep(121)
+            
+    except : 
+        print("FAILED : "+line)
+        ferr.write(line+"\n")
 
 fout.close()
